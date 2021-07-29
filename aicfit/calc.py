8e969118726c43d76882aedfed4b53e8f47d7775
nijigen-plot <quark_hardcore@yahoo.co.jp>
2021-07-29 23:35:06 +0900

    ポアソン分布を追加

--- aicfit/calc.py
+++ aicfit/calc.py
@@ -1,6 +1,9 @@
 import numpy as np
+from scipy.special import gammaln
+
 # 各分布の対数尤度とパラメータ偏微分値を計算
 
+# normal distribution
 def normal_log_likelihood(x, mu, var):
     return ((-(1/2) * np.log(2 * np.pi * var)) - ((x - mu)**2 / (2 * var))).sum()
 
@@ -9,8 +12,14 @@ def normal_derivative(x, mu, var):
     var_d = ((-1 / (2 * var)) + (((x - mu)**2) / (2 * (var**2) ))).sum()
     return mu_d, var_d
 
+# poisson distribution
+def poisson_log_likelihood(x, lambda_):
+    return (-lambda_ + ( x * np.log(lambda_)) - gammaln(x + 1)).sum()
 
+def poisson_derivative(x, lambda_):
+    return (1 + (x / lambda_))
 
+# cauchy distribution
 def cauchy_log_likelihood(x, mu, tau):
     # return (5 * np.log(tau)) - (10 * np.log(np.pi)) - (np.log( ((x - mu) ** 2) + tau )).sum()
     return (-np.log(np.pi) + (np.log(tau) / 2) - (np.log( ((x - mu) ** 2) + tau ))).sum()
